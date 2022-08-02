879d62a5a720d071f9691c1f449ec2168e069477
vcarine <you@example.com>
2022-08-02 15:49:01 +0200

    validation login1

--- Controllers/Security/SecurityController.php
+++ Controllers/Security/SecurityController.php
@@ -10,9 +10,33 @@ class SecurityController
      */
     public function login(): void
     {
+        $errors = [];
+//        dd($_SERVER);
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+//            var_dump('traitement des données');
+//            var_dump($_POST);
+//            dd($_POST);
+            // JE VAIS APPELER MON USERMANAGER ET APPELER SA FONCTION LOGIN =>  QUI VA DONNE UN RESULTAT
+
+            if (empty($_POST['email'])) {
+                $errors[] = "Veuillez choisir un email";
+            }
+            if (empty($_POST['password']) || strlen($password) < 6) {
+                $errors[] = "Veuillez choisir un password et mot de passe doit contenir au moins 6 caractères";
+            }
+            if (count($errors) == 0) {
+                $resultat = $this->userManager->login( $_POST['email'], $_POST['password']);
+//            var_dump($resultat);
+//            die();
+                if (!is_null($resultat)) {
+                    $_SESSION['user'] = serialize($resultat);
+                    header('Location: article.view.php');
+                } else {
+                    $errors = "les identifiants sont incorrectes";
+                }
+            }
+        }
+
         require "Views/security/login.php";
     }
-
-
-
 }
