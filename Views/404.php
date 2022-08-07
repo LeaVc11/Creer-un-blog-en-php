199910191b7931c4edc61b98026a4e40688aa6ee
vcarine <you@example.com>
2022-08-07 22:51:46 +0200

    AdminArticles

---
+++ Views/404.php
@@ -0,0 +1,7 @@
+
+<div id="content" class="container-fluid">
+    <main class="p-5">
+        <h2>Page non trouvée</h2>
+        <p>Erreur 404 : la page demandée n'a pas été trouvée</p>
+    </main>
+</div>
---
+++ Views/Admin/Admin.php
@@ -0,0 +1,61 @@
+
+
+<div class="d-flex justify-content-between">
+    <div id="admin-nav" class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark left-0">
+        <div>
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <span class="fs-4">Carine VINAGRE</span>
+            </a>
+        </div>
+        <hr>
+        <ul class="nav nav-pills flex-column mb-auto">
+            <li class="nav-item">
+                <a href="" class="nav-link active" aria-current="page">
+                    Tableau de bord
+                </a>
+            </li>
+            <li>
+                <a href="/admin/show-posts" class="nav-link text-white">
+                    Articles
+                </a>
+            </li>
+            <li>
+                <a href="" class="nav-link text-white">
+                    Ajouter un article
+                </a>
+            </li>
+
+            <li>
+                <a href="" class="nav-link text-white">
+                    Commentaires
+                </a>
+            </li>
+            <li>
+                <a href="" class="nav-link text-white">
+                    Utilisateurs
+                </a>
+            </li>
+        </ul>
+        <div class="dropdown">
+            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="https://via.placeholder.com/150" alt="" width="32" height="32" class="rounded-circle me-2">
+                <strong></strong>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+                <li><a class="dropdown-item" href="#">Paramètres</a></li>
+                <li><a class="dropdown-item" href="">Profil</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li>
+                    <form action="/" method="post">
+
+                        <button class="dropdown-item" type="submit">Déconnexion</button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <h1 class="display-6">Interface administrateur</h1>
+
+    </div>
+</div>
