b4147aec0059985f5caa6001b137c8dee2d98c3a
vcarine <you@example.com>
2022-08-26 16:36:48 +0200

    comment router

--- index.php
+++ index.php
@@ -1,21 +1,10 @@
 <?php
 
-
-use App\Controllers\AdminController;
-use App\Controllers\ArticlesController;
-use App\Controllers\SecurityController;
 use App\Routing\Router;
 
 require 'vendor/autoload.php';
 
 //dump($_SERVER);
-
-
-$url = null;
-$id = null;
-//var_dump(md5('johndoe'));die(); //prend le resultat du hashage
-//
-//var_dump($_SESSION['user']);
 
 $router = new Router($_GET['url']);
 
@@ -38,102 +27,5 @@ try {
 } catch (Exception $e) {
 }
 session_start();
-//
-//
-///**
-// * @return void
-// * @throws Exception
-// */
-//function getDisplayArticle(): void
-//{
-//    $articles = new ArticlesController();
-//    $articles->displayArticles();
-//}
-//
-///**
-// * @param string $parameter
-// * @param int $id
-// *
-// * @return void
-// *
-// * @throws Exception
-// */
-//function actionArticle(string $parameter, int $id): void
-//{
-//    $articles = new ArticlesController();
-//    if ($parameter === "homepage") {
-//        $articles->homePage();
-//    } else if ($parameter === "s") {
-//        $articles->showArticle($id);
-////    } else if ($parameter === "a") {
-////        $articles->addArticle();
-//
-////    } else if ($parameter === "e") {
-////        $articles->editArticle();
-////    } else if ($parameter === "d") {
-////        $articles->deleteArticle($id);
-//    } else {
-//        throw new Exception("La page n'existe pas");
-//    }
-//}
-//
-//function security(string $parameter): void
-//{
-//
-//    $controller = new SecurityController();
-//
-//    if ($parameter === 'login') {
-//        $controller->login();
-//    }
-//    if ($parameter === 'register') {
-//        $controller->register();
-//    }
-//    // J'appel la fonction logout de mon SecurityController
-//    if ($parameter == 'logout') {
-//        $controller->logout();
-//    }
-//}
-//
-///**
-// * @throws Exception
-// */
-//function admin($parameter): void
-//{
-//    $articles = new AdminController();
-//    if ($parameter === 'dashboard') {
-//        $articles->dashboard();
-//    } else if ($parameter === "s") {
-//        $articles->showArticle($id);
-//    } else if ($parameter === "a") {
-//        $articles->addArticles();
-//    } else if ($parameter === "e") {
-//        $articles->editArticle();
-//    } else if ($parameter === "d") {
-//        $articles->deleteArticle($id);
-//    } else {
-//        throw new Exception("La page n'existe pas");
-//    }
-//
-//}
-//
-////function home($parameter):void{
-////    $homecontroller = new HomeController();
-////    if ($parameter === 'home')
-////    $homecontroller->displayDashboard();
-////
-////}
-//
-//function errors($parameter): void
-//{
-//    $controller = new ExceptionController();
-//    if ($parameter === 'errors') {
-//        $controller->pageIntrouvable();
-//    } else {
-//        throw new Exception("La page n'existe pas");
-//    }
-//}
-
-
-
 
 
