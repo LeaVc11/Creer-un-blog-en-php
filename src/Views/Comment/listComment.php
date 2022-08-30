b34f4bd285b60a01e79be22a90bb56760556ad46
vcarine <you@example.com>
2022-08-30 18:56:42 +0200

    router editComment

--- src/Views/Comment/listComment.php
+++ src/Views/Comment/listComment.php
@@ -25,8 +25,36 @@
 </table>
 <div class=" text-center">
     <a href="<?= Router::generate('/articles/') ?>" class="btn btn-primary text-center text-white fw-bold mb-2" target="_blank">Retour</a>
-    <a href="<?= Router::generate('/comments/'.$comment->getId()) ?>" class="btn btn-warning text-center text-white fw-bold mb-2" target="_blank">Modifier</a>
 </div>
+<div class=" m-4 fw-bold ">
+
+    <h1 class="m-3 text-center text-primary">Modifier un commentaire</h1>
+    <form method="POST" action="" enctype="multipart/form-data">
+        <div class="row m-5">
+            <div class="col text-primary fw-bold ">
+                <label for="title">Titre : </label>
+                <input type="text" class="form-control" id="title" name="title">
+            </div>
+        </div>
+        <div class="row m-5">
+            <div class="col text-primary fw-bold ">
+                <label for="author">Auteur: </label>
+                <input type="text" class="form-control" id="author" name="author">
+            </div>
+        </div>
+        <div class="row m-5">
+            <div class="col text-primary fw-bold">
+                <label for="content">Content: </label>
+                <textarea name="content" class="form-control" id="content" cols="30" rows="10"></textarea>
+            </div>
+        </div>
+        <div class=" text-center">
+            <a href="admin/dashboard" class="btn btn-primary text-white text-center mb-2 w-100 rounded-1 border form-control" target="_blank">Retour</a>
+            <button class="btn btn-primary text-white text-center w-100 rounded-1 border form-control"
+                    type="submit">Valider
+            </button>
+        </div>
+    </form>
 
 
 
