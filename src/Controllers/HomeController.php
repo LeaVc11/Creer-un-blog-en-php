003764ff315320c30f792cd4e0a586fce4e54fd3
vcarine <you@example.com>
2022-08-30 08:12:31 +0200

    pb AbstractController ligne 15

---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class HomeController extends AbstractController
+{
+    public function index(): void
+    {
+        $this->render('accueil.view');
+    }
+}
