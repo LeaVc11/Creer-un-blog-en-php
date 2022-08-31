344140e3074671f0bc5ea5ffecdf3c6c25bde72b
vcarine <you@example.com>
2022-08-31 17:11:38 +0200

    comment status

--- src/Views/Admin/dashboard.php
+++ src/Views/Admin/dashboard.php
@@ -4,106 +4,111 @@ use App\Routing\Router;
 
 ?>
 <!--Bienvenue-->
-<h2 class="text-center fw-bold m-3">Bonjour et bienvenue sur le blog <?= $user->getUsername() ?></h2>
+<h2 class="text-center fw-bold m-3">Bonjour et bienvenue sur le blog <span
+            class="text-decoration-underline"> <?= $user->getUsername() ?></span></h2>
 
-<h2 class="text-secondary m-5 ">Articles</h2>
-<p class="lead">Administrez ici les articles du blog.</p>
+<h2 class=" text-center text-secondary m-5 ">Articles</h2>
+<p class=" text-center lead">Administrez ici les articles du blog.</p>
 
+<div class="container">
+    <table class="table text-center">
+        <tr class="table-dark">
+        <tr>
+            <th scope="col">Titre</th>
+            <th scope="col">Image</th>
+            <th scope="col">Chapô</th>
+            <th scope="col">Slug</th>
+            <th scope="col">Content</th>
+            <th scope="col">Date de création</th>
+            <th scope="col">Date de modification</th>
+            <th scope="col">Auteur</th>
+            <?php
 
-<table class="table text-center">
-    <tr class="table-dark">
-    <tr>
-        <th scope="col">Titre</th>
-        <th scope="col">Image</th>
-        <th scope="col">Chapô</th>
-        <th scope="col">Slug</th>
-        <th scope="col">Content</th>
-        <th scope="col">Date de création</th>
-        <th scope="col">Date de modification</th>
-        <th scope="col">Auteur</th>
-        <?php
+            if (isset($_SESSION['user'])) {
+                echo('<th>Edition</th>');
+            }
+            ?>
+        </tr>
+        <div class="d-flex justify-content-end">
+            <a href="admin/add" class="btn btn-warning  m-1">Ajouter</a>
+        </div>
+        <tr>
+            <?php
+            foreach ($articles
 
-        if (isset($_SESSION['user'])) {
-            echo('<th>Edition</th>');
-        }
-        ?>
-    </tr>
-    <div class="d-flex justify-content-end">
-        <a href="admin/add" class="btn btn-warning  m-1">Ajouter</a>
-    </div>
-    <tr>
-        <?php
-        foreach ($articles as $article) {
-        ?>
-    <tr>
-        <td><?= $article->getImageLink() ?></td>
-        <td><?= $article->getChapo() ?></td>
-        <td><?= $article->getTitle() ?></td>
-        <td><?= $article->getContent() ?></td>
-        <td><?= $article->getSlug() ?></td>
-        <td><?= $article->getAuthor() ?></td>
-        <td><?= $article->getCreatedAt()->format('d/m/Y ') ?></td>
-        <td><?= $article->getUpdatedAt()->format('d/m/Y ') ?></td>
+            as $article) {
+            ?>
+        <tr>
+            <td><?= $article->getImageLink() ?></td>
+            <td><?= $article->getChapo() ?></td>
+            <td><?= $article->getTitle() ?></td>
+            <td><?= $article->getContent() ?></td>
+            <td><?= $article->getSlug() ?></td>
+            <td><?= $article->getAuthor() ?></td>
+            <td><?= $article->getCreatedAt()->format('d/m/Y ') ?></td>
+            <td><?= $article->getUpdatedAt()->format('d/m/Y ') ?></td>
 
-    <td>
+            <td>
 
-        <a href="<?= Router::generate('/admin/editArticle/'.$article->getId())?>" class="btn btn-secondary"
-           role="button">Modifier</a>
-    </td>
-    <td>
+                <a href="<?= Router::generate('/admin/editArticle/' . $article->getId()) ?>" class="btn btn-secondary"
+                   role="button">Modifier</a>
+            </td>
+            <td>
 
-        <a href="<?= Router::generate('/admin/deleteArticle/'.$article->getId()) ?>" class="btn btn-danger"
-           role="button">Supprimer</a>
-    </td>
+                <a href="<?= Router::generate('/admin/deleteArticle/' . $article->getId()) ?>" class="btn btn-danger"
+                   role="button">Supprimer</a>
+            </td>
 
-    <?php } ?>
-    </tr>
-</table>
+            <?php } ?>
+        </tr>
+    </table>
+</div>
+<h2 class="text-center text-secondary m-5 ">Commentaires</h2>
+<p class="text-center lead">Administrez ici les commentaires de l'articles.</p>
 
-<h2 class="text-secondary m-5 ">Commentaires</h2>
-<p class="lead">Administrez ici les commentaires de l'articles.</p>
+<div class="container">
+    <table class="table text-center">
+        <tr class="table-dark">
+        <tr>
+            <th scope="col">Titre</th>
+            <th scope="col">Status</th>
+            <th scope="col">Content</th>
+            <th scope="col">Auteur</th>
+            <th scope="col">Date de création</th>
 
+            <?php
 
-<table class="table text-center">
-    <tr class="table-dark">
-    <tr>
-        <th scope="col">Titre</th>
-        <th scope="col">Status</th>
-        <th scope="col">Content</th>
-        <th scope="col">Auteur</th>
-        <th scope="col">Date de création</th>
+            if (isset($_SESSION['user'])) {
+                echo('<th>Edition</th>');
+            }
+            ?>
+        </tr>
+        <tr>
+            <?php
+            //        var_dump($comments as $comment);
+            //        die();
+            foreach ($comments
 
-        <?php
+            as $comment) {
+            ?>
+        <tr>
+            <td><?= $comment->getTitle() ?></td>
+            <td><?= $comment->getStatus() ?></td>
+            <td><?= $comment->getContent() ?></td>
+            <td><?= $comment->getCreatedBy() ?></td>
+            <td><?= $comment->getCreatedAt()->format('d/m/Y') ?>
+            </td>
 
-        if (isset($_SESSION['user'])) {
-            echo('<th>Edition</th>');
-        }
-        ?>
-    </tr>
-    <tr>
-        <?php
-//        var_dump($comments as $comment);
-//        die();
-        foreach ($comments as $comment) {
-        ?>
-    <tr>
-        <td><?= $comment->getTitle() ?></td>
-        <td><?= $comment->getStatus() ?></td>
-        <td><?= $comment->getContent() ?></td>
-        <td><?= $comment->getCreatedBy() ?></td>
-        <td><?= $comment->getCreatedAt()->format('d/m/Y') ?>
-        </td>
+            <td>
+                <a href="<?= Router::generate('/comments/addComment/') ?>" class="btn btn-secondary"
+                   role="button">Valider</a>
+            </td>
+            <td>
 
-        <td>
-            <a href="<?= Router::generate('/comments/addComment/')?>" class="btn btn-secondary"
-               role="button">Valider</a>
-        </td>
-        <td>
+            </td>
 
-        </td>
-
-        <?php } ?>
-    </tr>
-</table>
-
+            <?php } ?>
+        </tr>
+    </table>
+</div>
 
