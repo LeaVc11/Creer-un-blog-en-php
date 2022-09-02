54ff43af7444384266faf501d0b438e9f54a2431
vcarine <you@example.com>
2022-09-02 20:03:26 +0200

    message

--- src/Controllers/ContactController.php
+++ src/Controllers/ContactController.php
@@ -37,36 +37,5 @@ class ContactController extends AbstractController
         $this->render('Contact/formContact');
 
     }
-    private function getFormErrors($id = null): array
-    {
-        $errors = [];
-        if (empty($_POST['title'])) {
-            $errors[] = 'Veuillez saisir un titre';
-            $article = $this->articleManager->getByTitle($_POST['title']);
 
-            if (!is_null($article) && $article->getId() != $id) {
-                $errors[] = 'Un article avec ce titre existe déjà !';
-            }
-        }
-
-        if (empty($_POST['chapo'])) {
-            $errors[] = 'Veuillez saisir un chapo';
-        }
-
-        if (empty($_POST['content'])) {
-            $errors[] = 'Veuillez saisir du contenu';
-        }
-
-        if (empty($_POST['author'])) {
-            $errors[] = 'Veuillez saisir un auteur';
-        }
-
-        if (empty($_POST['slug'])) {
-            $errors[] = 'Veuillez saisir un slug';
-        }
-
-        $_SESSION['flash'] = array_merge($_SESSION['flash'], $errors);
-
-        return $errors;
-    }
 }
